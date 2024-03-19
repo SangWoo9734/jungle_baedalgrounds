72c730ef26ab17703fcf7168341d13b2551b93d1
Parksangwoo <psu8430@gmail.com>
2024-03-19 14:32:54 +0900

    feat: login/signIn 페이지 마크업 추가

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+LOGO_URL = './images/logo.png'
+
 # 메인 페이지
 @app.route('/')
 def deliveryBoardPage():
   return render_template('main.html')
 
-# 로그인 / 회원가입
+# 로그인
 @app.route('/login')
 def loginPage():
-  return render_template('login.html')
+  return render_template('login.html', image =LOGO_URL)
+
+@app.route('/signIn')
+def signInPage():
+  return render_template('signIn.html', image=LOGO_URL)
 
 if __name__ == '__main__':  
   app.run('0.0.0.0',port=5000,debug=True)
