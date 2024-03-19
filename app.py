b655451e89259f23f60b4f015aec8a13cb6da9a6
Parksangwoo <psu8430@gmail.com>
2024-03-19 20:40:46 +0900

    feat: 템플릿에 필요한 인자 전달

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+# CONTANTS
+SURVICE_TITLE = "BAEDALGROUNDS"
 LOGO_URL = './images/logo.png'
 
 # 메인 페이지
 @app.route('/')
 def deliveryBoardPage():
-  return render_template('main.html')
+  return render_template('main.html', service_title=SURVICE_TITLE, image =LOGO_URL)
 
 # 로그인
 @app.route('/login')
 def loginPage():
-  return render_template('login.html', image =LOGO_URL)
+  return render_template('login.html', service_title=SURVICE_TITLE, image =LOGO_URL)
 
 @app.route('/signIn')
 def signInPage():
-  return render_template('signIn.html', image=LOGO_URL)
+  return render_template('signIn.html', service_title=SURVICE_TITLE, image=LOGO_URL)
 
 if __name__ == '__main__':  
-  app.run('0.0.0.0',port=5000,debug=True)
+  app.run('0.0.0.0',port=5001,debug=True)
